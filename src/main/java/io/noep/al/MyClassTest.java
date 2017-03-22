7921203784b222420f8df2b44ded15ba9af52956
Taehoon Yoo <noep@naver.com>
2017-03-23 01:10:53 +0900

    init

---
+++ src/main/java/io/noep/al/MyClassTest.java
@@ -0,0 +1,18 @@
+package io.noep.al;
+
+/**
+ * Created by Taehoon Yoo
+ * User  : taehoon
+ * Date  : 2017. 3. 23.
+ * Time  : 오전 1:10
+ * Page  : http:noep.github.io
+ * Email : noep@naver.com
+ * Desc  :
+ */
+public class MyClassTest {
+
+    public void print() {
+        System.out.println("print");
+    }
+
+}
---
+++ src/test/java/io/noep/al/MyTest.java
@@ -0,0 +1,26 @@
+package io.noep.al;
+
+
+import org.junit.*;
+
+/**
+ * Created by Taehoon Yoo
+ * User  : taehoon
+ * Date  : 2017. 3. 23.
+ * Time  : 오전 1:03
+ * Page  : http:noep.github.io
+ * Email : noep@naver.com
+ * Desc  :
+ */
+
+public class MyTest {
+
+    @Test
+    public void test() {
+
+        System.out.println("hello");
+        MyClassTest myClassTest = new MyClassTest();
+        myClassTest.print();
+
+    }
+}
